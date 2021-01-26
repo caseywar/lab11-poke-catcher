ef25d85c96bd403d0c19e40661ecd600fccc556b
Casey Warren <caseywar1017@gmail.com>
2021-01-26 17:49:20 -0500

    added render table row and render seen results functions to display results on the results page

--- results/results-utils.js
+++ results/results-utils.js
@@ -1,23 +1,50 @@
 // import { getPokeStats } from '../localStorageUtils.js';
 import { findByUnderscoreId } from '../utils.js';
 import pokemon from '../data.js';
+// import { getPokeStats } from '../localStorageUtils.js';
 
 export function renderTableRows(pokemonInArray) {
-    const row = document.createElement('tr');
-    const imageTd = document.createElement('td');
-    const image = document.createElement('img');
-    const nameTd = document.createElement('td');
-    const seenTd = document.createElement('td');
-    const caughtTd = document.createElement('td');
+    if (pokemonInArray.caught > 0) {
+        const row = document.createElement('tr');
+        const imageTd = document.createElement('td');
+        const image = document.createElement('img');
+        const nameTd = document.createElement('td');
+        const seenTd = document.createElement('td');
+        const caughtTd = document.createElement('td');
 
-    const rowItem = findByUnderscoreId(pokemon, pokemonInArray._id);
+        const rowItem = findByUnderscoreId(pokemon, pokemonInArray._id);
 
-    image.src = rowItem.url_image;
-    imageTd.appendChild(image);
-    nameTd.textContent = rowItem.pokemon;
-    seenTd.textContent = pokemonInArray.seen;
-    caughtTd.textContent = pokemonInArray.caught;
+        image.src = rowItem.url_image;
+        imageTd.appendChild(image);
+        nameTd.textContent = rowItem.pokemon;
+        seenTd.textContent = pokemonInArray.seen;
+        caughtTd.textContent = pokemonInArray.caught;
 
-    row.append(imageTd, nameTd, seenTd, caughtTd);
-    return row;
+        row.append(imageTd, nameTd, seenTd, caughtTd);
+        return row;
+    } else {
+        return [];
+    }
+}
+
+
+export function renderSeenResults(pokemonInArray) {
+    if (pokemonInArray.caught === 0) {
+        const row = document.createElement('tr');
+        const imageTd = document.createElement('td');
+        const image = document.createElement('img');
+        const nameTd = document.createElement('td');
+        const seenTd = document.createElement('td');
+
+        const rowItem = findByUnderscoreId(pokemon, pokemonInArray._id);
+        image.src = rowItem.url_image;
+        imageTd.appendChild(image);
+        nameTd.textContent = rowItem.pokemon;
+        seenTd.textContent = pokemonInArray.seen;
+        row.append(imageTd, nameTd, seenTd,);
+        return row;
+    } else {
+
+        return [];
+    }
 }
--- results/results.js
+++ results/results.js
@@ -1,17 +1,21 @@
 import { getPokeStats, setPokeStats } from '../localStorageUtils.js';
-import { renderTableRows } from './results-utils.js';
+import { renderTableRows, renderSeenResults } from './results-utils.js';
 
 const table = document.getElementById('caught-table');
 const resetButton = document.getElementById('reset-button');
 const currentStats = getPokeStats();
+const SeenTable = document.getElementById('seen-table');
+
 
 for (let stat of currentStats) {
     table.append(renderTableRows(stat));
+}
+for (let stat of currentStats) {
+    SeenTable.append(renderSeenResults(stat));
 }
 
 resetButton.addEventListener('click', () => {
     const emptyArray = [];
     setPokeStats(emptyArray);
     window.location = '../';
-    console.log('hey');
 });
