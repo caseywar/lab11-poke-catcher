f3d51fdfc0941848fc099fe3ebdbbf303a934c6d
Casey Warren <caseywar1017@gmail.com>
2021-01-25 19:15:47 -0500

    three console logs logging when set three pokemon and render poke image functions run

--- utils.js
+++ utils.js
@@ -16,13 +16,13 @@ export function findByUnderscoreId(array, id) {
 }
 
 export function setThreePokemon() {
-    numberOfTurns++;
+    // numberOfTurns++;
 
     let pokeOne = getRandomPokemon();
     let pokeTwo = getRandomPokemon();
     let pokeThree = getRandomPokemon();
 
-    const anyOfThePokemonMatch = pokeOne._id === pokeTwo._id || pokeOne._id === pokeThree._id || pokeTwo._id === pokeThree._id);
+    const anyOfThePokemonMatch = pokeOne._id === pokeTwo._id || pokeOne._id === pokeThree._id || pokeTwo._id === pokeThree._id;
 
     while (anyOfThePokemonMatch) {
         pokeOne = getRandomPokemon();
@@ -30,8 +30,28 @@ export function setThreePokemon() {
         pokeThree = getRandomPokemon();
 
     }
+    const img1 = renderPokeImage(pokeOne._id);
+    const img2 = renderPokeImage(pokeTwo._id);
+    const img3 = renderPokeImage(pokeThree._id);
 
+    const div = document.getElementById('pokemon');
 
+    // div.textContent = '';
 
+    // div.append(img1, img2, img3);
 
+}
+
+export function renderPokeImage(pokemonItem) {
+    const image = document.createElement('img');
+    image.src = pokemonItem.url_image;
+
+    image.classList.add('poke-img');
+    // image.addEventListener('click', () => {
+    //     incrementCaught(pokemonItem._id);
+
+    // });
+    console.log('sup');
+
+    return image;
 }
